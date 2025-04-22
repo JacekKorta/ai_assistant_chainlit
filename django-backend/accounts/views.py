6979a8d424439b3bab065e5dbf43d60ccaf656ab
JacekKorta <jacek.korta@gmail.com>
2025-04-22 18:37:39 +0200

    Update tests

--- django-backend/accounts/views.py
+++ django-backend/accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
